34608e906dc2998a6639dfcc033ad07289b2351f
Max <goooglemax1993@gmail.com>
2025-10-22 21:55:16 +0200

    fix usage serverID  (#15)
    * add-docker: fix usage ContextWithServerID.

--- cmd/local-chain/main.go
+++ cmd/local-chain/main.go
@@ -40,7 +40,7 @@ var (
 func main() {
 	fmt.Println("raftBootstrap:", bootstrap)
 	logger := slog.Default()
-	ctx := pkg.ContextWithServerID(context.Background(), raft.ServerID(raftAddr))
+	ctx := pkg.ContextWithServerID(context.Background(), raft.ServerID(nodeID))
 	cfg, err := NewConfig(serverID)
 	if err != nil {
 		log.Printf("error prepare configs: %v", err)
